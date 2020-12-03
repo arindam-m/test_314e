fb7b02bf7daed0f426067b078401e69245a7c836
Arindam Mondal <arindam.mondal.006@gmail.com>
2020-12-03 23:25:56 +0530

    Added a unit-test

--- app_scraper/tests.py
+++ app_scraper/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
+from . import scrape_logic as sl
+
 # Create your tests here.
+
+class TestScrapeLogic(TestCase):
+
+    def setUp(self):
+        self.url = "https://www.314e.com/"
+        self.level = 1
+
+    def test_url_crawler(self):
+        result = sl.url_crawler(self.url, self.level)
+        self.assertEqual(len(result), 1)
