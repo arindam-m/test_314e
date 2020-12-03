53d29bb8bb2336f679a18ac69372c31eb490a865
Arindam Mondal <arindam.mondal.006@gmail.com>
2020-12-04 03:03:26 +0530

    Still trying S_KEY to make it work

--- project_main/settings.py
+++ project_main/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '&05!0n03ay-41^63&#!jfk6n!v6uft=pwf7vvo4p&dcny5)zvp'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = '&05!0n03ay-41^63&#!jfk6n!v6uft=pwf7vvo4p&dcny5)zvp'
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DJANGO_DEBUG_VALUE') == 'True')
