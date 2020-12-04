9badde9b7db25e03fce30a3dac27e38dbbfd4671
Arindam Mondal <arindam.mondal.006@gmail.com>
2020-12-04 15:04:55 +0530

    Trying not to expose S_KEY

--- project_main/settings.py
+++ project_main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,8 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&05!0n03ay-41^63&#!jfk6n!v6uft=pwf7vvo4p&dcny5)zvp'
-# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+if not SECRET_KEY:
+    SECRET_KEY = sys.argv[0]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DJANGO_DEBUG_VALUE') == 'True')
