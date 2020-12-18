7bb53deb51dbff6e0f44aa34dd9c208b20cde347
Arindam Mondal <arindam.mondal.006@gmail.com>
2020-12-18 16:43:00 +0530

    A few more cleanups

--- app_scraper/logics/listing_urls_spider.py
+++ app_scraper/logics/listing_urls_spider.py
@@ -9,9 +9,15 @@ import os
 import scrapy
 from scrapy.crawler import CrawlerProcess
 
-WEBPAGE_LINKS = []
-
 logics_dir = os.getcwd() + '\\app_scraper\\logics\\'
+
+json_input_file = logics_dir + "input_post_data.json"
+
+with open(json_input_file, encoding='utf8') as json_data:
+    input_data_dict = json.load(json_data)
+
+root_index = input_data_dict['root_url']
+WEBPAGE_LINKS = [root_index]
 
 
 class ListingURLs(scrapy.Spider):
@@ -20,16 +26,8 @@ class ListingURLs(scrapy.Spider):
 
     global WEBPAGE_LINKS
 
-    json_input_file = logics_dir + "input_post_data.json"
-
-    with open(json_input_file, encoding='utf8') as json_data:
-        input_data_dict = json.load(json_data)
-
-    # root_index = 'https://www.314e.com/'
-    root_index = input_data_dict['root_url']
     start_urls = [root_index]
 
-    WEBPAGE_LINKS = [root_index]
     links_to_ignore = ['#', 'javascript:void(0);', root_index]
 
     custom_settings = {
@@ -55,14 +53,16 @@ class ListingURLs(scrapy.Spider):
 
         for link in response.css('a::attr(href)').getall():
             if (link not in self.links_to_ignore
-                    and link.startswith(self.root_index)
+                    and link.startswith(root_index)
                     and link not in WEBPAGE_LINKS):
                 WEBPAGE_LINKS.append(link)
 
 
-process = CrawlerProcess()
-process.crawl(ListingURLs)
-process.start()
+if input_data_dict['depth_level'] > 0:
+
+    process = CrawlerProcess()
+    process.crawl(ListingURLs)
+    process.start()
 
 
 output_file = logics_dir + "url_data.json"
