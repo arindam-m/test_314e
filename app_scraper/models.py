baf6b3a24f85d2c4aaa44127eb9825d9ff1d321d
Arindam Mondal <arindam.mondal.006@gmail.com>
2021-01-03 05:38:27 +0530

    Added a placeholder model

--- app_scraper/models.py
+++ app_scraper/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class VisualData(models.Model):
+
+    class LevelEnum(models.IntegerChoices):
+        ZERO = 0, 'Root (Home Page)'
+        ONE = 1, 'First'
+        TWO = 2, 'Second'
+        THREE = 3, 'Third'
+        FOUR = 4, 'Fourth'
+
+    url = models.CharField(max_length=256)
+    level = models.IntegerField(default=LevelEnum.ZERO,
+                                choices=LevelEnum.choices)
+    num_of_urls = models.IntegerField()
+    data_frequency = models.JSONField()
+
+    def __str__(self):
+        return "A VisualData object has been created"
