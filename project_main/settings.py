2b47c1c9ee48f002db01450cf284dc166488002d
Arindam Mondal <arindam.mondal.006@gmail.com>
2021-01-15 06:01:03 +0530

    Using SQLite for CI test

--- project_main/settings.py
+++ project_main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import sys
 from pathlib import Path
 
 from django.core.management.utils import get_random_secret_key
@@ -83,23 +84,25 @@ WSGI_APPLICATION = 'project_main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dcc3835153l06r',
-        'USER': 'kcxvoltcimoqzg',
-        'PASSWORD': '9cda699d091868a2d11dcedbfe9ff20ee7e2f8c28b3cc6b0b8a96b9e6ae180ca',
-        'HOST': 'ec2-107-22-241-205.compute-1.amazonaws.com',
-        'PORT': '5432',
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'dcc3835153l06r',
+            'USER': 'kcxvoltcimoqzg',
+            'PASSWORD': '9cda699d091868a2d11dcedbfe9ff20ee7e2f8c28b3cc6b0b8a96b9e6ae180ca',
+            'HOST': 'ec2-107-22-241-205.compute-1.amazonaws.com',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
